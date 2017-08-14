efe084ebdfbfb3a1c145b007eb27830bb27527e7
Neel Smith <dnsmith.neel@gmail.com>
2017-08-14 08:12:33 -0400

    Update API docs

--- src/main/scala/DatedWeek.scala
+++ src/main/scala/DatedWeek.scala
@@ -1,7 +1,17 @@
 package edu.holycross.shot.coursecal
 
+
+/** Association of one week of daily topics with a week of
+* calendrical data for a given class meeting pattern.
+*
+* @param topics A [[Week]] of course topics.
+* @param dates One week of calendar dates for a course meeting pattern.
+*/
 case class DatedWeek(topics: Week, dates: CourseWeek) {
 
+  /** Format a string representing a week on a course calendar
+  * as one line of a markdown table.
+  */
   def calendarString: String = {
     dates match {
       case tth : TuThWeek => tthString
@@ -10,6 +20,9 @@ case class DatedWeek(topics: Week, dates: CourseWeek) {
     }
   }
 
+  /** Format a string representing a week on a Tues-Thurs course calendar
+  * as one line of a markdown table.
+  */
   def tthString: String = {
     dates match {
       case tth : TuThWeek => s"${shortDisplayDay(tth.tues)}-${shortDisplayDay(tth.thurs)} | ${topics.entries(0)} | ${topics.entries(1)}"
@@ -17,6 +30,10 @@ case class DatedWeek(topics: Week, dates: CourseWeek) {
       case wf : WedFriWeek => ""
     }
   }
+
+  /** Format a string representing a week on a Mon-Wed-Fri course calendar
+  * as one line of a markdown table.
+  */
   def mwfString: String = {
     dates match {
       case tth : TuThWeek => ""
@@ -24,6 +41,10 @@ case class DatedWeek(topics: Week, dates: CourseWeek) {
       case wf : WedFriWeek => ""
     }
   }
+
+  /** Format a string representing a week on a Wed-Fri course calendar
+  * as one line of a markdown table.
+  */
   def wfString: String = {
     dates match {
       case tth : TuThWeek =>  ""
